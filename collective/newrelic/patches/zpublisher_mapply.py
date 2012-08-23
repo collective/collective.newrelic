eb2c8f58b04aec60e816a4f4c7867a2654f4cd82
Jan Branbergen <branbergen@gw20e.com>
2012-08-23 17:27:36 +0200

    Typo in zpublisher_mapply.py

--- collective/newrelic/patches/zpublisher_mapply.py
+++ collective/newrelic/patches/zpublisher_mapply.py
@@ -17,7 +17,7 @@ def newrelic_mapply(object, positional=(), keyword={},
     with newrelic.api.function_trace.FunctionTrace(trans,
                                                    name=object.__class__.__name__, 
                                                    group='Zope'):
-        result = orginal_mapply(object, positional, keyword, debug, maybe, missing_name, handle_class, context, bind)
+        result = original_mapply(object, positional, keyword, debug, maybe, missing_name, handle_class, context, bind)
 
     return result
 
