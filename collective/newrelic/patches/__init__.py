e17c229ca4c88ccb9ec6e8957e9eededa39c8a6d
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2013-03-22 11:48:06 +0100

    Remove executable mode from all files. Improved patching to match upcoming fix of newrelic. Should fix problem with thread locking itself

--- collective/newrelic/patches/__init__.py
+++ collective/newrelic/patches/__init__.py
@@ -3,7 +3,8 @@ __import__('pkg_resources').declare_namespace(__name__)
 import newrelic.agent
 
 #Patch newrelic_application to work with a threading.local for early (own thread!) lock checking
-import newrelic_application
+# import newrelic_application
+import newrelic_transaction
 
 # This is one is required: it creates the 'webtransaction'
 import zserverpublisher
